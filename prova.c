4d3b72f1dd8e9d5f0b3bc15a0538ebf038f045c8
giamma <giamma71@gmail.com>
2021-05-30 22:52:52 +0200

    completato

--- prova.c
+++ prova.c
@@ -2,13 +2,10 @@
 
 int main(int argc, char const *argv[])
 {
+    printf("prima rigna");
     int a=0;
     printf("dammi un mnumero\n");
     scanf("%i",&a);
     printf("%i\n",a);
-    #prova altro
-    #secondo
-    #tt
-    ##nnnnnnnnnnnnnnnn
     return 0;
 }
